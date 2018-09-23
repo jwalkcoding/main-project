e36d13a2e51b90b9f5237af353636459124e8f17
Jay Walker <jwalker.nationwide@gmail.com>
2018-09-23 11:31:30 -0400

    removed devpack

--- nwDevpack/module.js
+++ nwDevpack/module.js
@@ -3,7 +3,10 @@
 /* using creating a subtree from existing folder in your repo
 
 1 split common folder into its own branch 
+     git subtree split --prefix nwDevpack --branch nwDevpack
 2. remove the common folder from your project
+    git rm -rf nwDevpack
 3. create a new repo within parent project by creating a folder and doing a git init
+    
 4 pull the contents of the branch you split  into the new repo with your parent project  */
 
