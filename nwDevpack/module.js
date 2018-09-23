960230f22959b599b9c9d742ea3808581d88f59f
Jay Walker <jwalker.nationwide@gmail.com>
2018-09-23 11:27:52 -0400

    added common tools folder

---
+++ nwDevpack/module.js
@@ -0,0 +1,9 @@
+//version 1.0 
+
+/* using creating a subtree from existing folder in your repo
+
+1 split common folder into its own branch 
+2. remove the common folder from your project
+3. create a new repo within parent project by creating a folder and doing a git init
+4 pull the contents of the branch you split  into the new repo with your parent project  */
+
